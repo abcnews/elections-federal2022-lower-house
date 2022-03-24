9997f0ca8b431117748a819c361c978b2734d29f
Colin Gourlay <colin@colin-gourlay.com>
2022-03-24 16:17:31 +1000

    Add hexes to Cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,17 +5,13 @@ import {
   ElectorateID,
   ELECTORATES,
   ELECTION_YEARS_PRIMARY_ALLIANCES,
-  DEFAULT_ELECTION_YEAR
+  DEFAULT_ELECTION_YEAR,
+  DEFAULT_RELATIVE_ELECTION_YEAR
 } from '../../lib/constants';
 import type { LiveResultsElectorate } from '../../lib/data';
 import { fetchLiveResultsElectorates, getLiveResultsElectorateAllocation } from '../../lib/data';
 import pastWinners from './past-winners.json';
 import styles from './styles.scss';
-
-interface CardProps {
-  electorateID: string;
-  hasResult?: boolean;
-}
 
 const MONTH_SHORTNAMES = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -28,6 +24,30 @@ const isToday = (date: Date, todayDate = new Date()) => {
     isThisYear(date, todayDate) && date.getMonth() === todayDate.getMonth() && date.getDate() === todayDate.getDate()
   );
 };
+
+interface HexProps {
+  allocation?: Allocation;
+  relativeAllocation?: Allocation;
+}
+
+const Hex: React.FC<HexProps> = ({ allocation, relativeAllocation }) => (
+  <div
+    className={styles.hex}
+    data-allocation={allocation || undefined}
+    data-relative-allocation={relativeAllocation || undefined}
+  >
+    <svg viewBox="0 0 51.3012701892 58">
+      <g transform="translate(4 4)">
+        <polygon points="0,37.5 21.65,50 43.3,37.5 43.3,12.5 21.65,0 0,12.5 0,37.5"></polygon>
+      </g>
+    </svg>
+  </div>
+);
+
+interface CardProps {
+  electorateID: string;
+  hasResult?: boolean;
+}
 
 const Card: React.FC<CardProps> = ({ electorateID, hasResult }) => {
   const electorate: Electorate = ELECTORATES.find(({ id }) => ElectorateID[electorateID] === id) as Electorate;
@@ -49,15 +69,24 @@ const Card: React.FC<CardProps> = ({ electorateID, hasResult }) => {
 
   const timeUpdated = result?.updated ? new Date(result.updated) : null;
 
+  const electoratePastWinners = pastWinners[electorateID];
+  const allocation = result ? undefined : undefined;
+  const relativeAllocation = electoratePastWinners[DEFAULT_RELATIVE_ELECTION_YEAR]
+    ? Allocation[electoratePastWinners[DEFAULT_RELATIVE_ELECTION_YEAR]]
+    : undefined;
+
   return (
     <div className={styles.root}>
       <div className={styles.flex}>
-        <h4 className={styles.title}>{electorate.name}</h4>
+        <h4 className={styles.title}>
+          <Hex allocation={allocation} relativeAllocation={relativeAllocation} />
+          {electorate.name}
+        </h4>
         {hasResult ? timeUpdated && <div>{`Updated ${formatTimeUpdated(timeUpdated)}`}</div> : <div />}
       </div>
-      {!hasResult && (
+      {!hasResult && Object.keys(electoratePastWinners).length > 0 && (
         <div className={styles.pastWinners}>
-          {Object.entries<Allocation>(pastWinners[electorateID])
+          {Object.entries<Allocation>(electoratePastWinners)
             .reverse()
             .map(([year, allocation]) => (
               <div key={year} className={styles.pastWinner} title={allocation} data-allocation={Allocation[allocation]}>
