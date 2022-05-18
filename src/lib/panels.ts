2d26570fabe40105547689818481b5b9f5ad4d91
Colin Gourlay <colin@colin-gourlay.com>
2022-05-18 14:12:42 +1000

    Panel text will only apply colour styling to electorate names that have been emphasised (bold/italic). Properly apply certainty data attributes to them instead of relative allocations.

--- src/lib/panels.ts
+++ src/lib/panels.ts
@@ -5,12 +5,19 @@ import blockStyles from '../components/Block/styles.scss';
 import type { GraphicProps, PossiblyEncodedGraphicProps } from '../components/Graphic';
 
 const SORTED_ELECTORATES = ELECTORATES.sort((a, b) => b.name.length - a.name.length);
+const EMPHASISED_ELEMENTS_SELECTOR = 'strong,em,b,i';
 
 export function applyColourToPanels(panels: PanelDefinition<PossiblyEncodedGraphicProps>[]) {
   const electorateIntroductionTracker: { [key: string]: boolean } = {};
 
   panels.forEach(({ data, nodes }) => {
-    const textNodes = nodes.reduce<Node[]>((memo, node) => memo.concat(textNodesUnder(node)), []);
+    const elements = nodes.filter(node => node.nodeType === Node.ELEMENT_NODE) as Element[];
+    const emphasisedElements = elements.reduce(
+      (memo, element) => memo.concat(Array.from(element.querySelectorAll(EMPHASISED_ELEMENTS_SELECTOR))),
+      [] as Element[]
+    );
+    const textNodes = emphasisedElements.reduce((memo, node) => memo.concat(textNodesUnder(node)), [] as Node[]);
+    // const textNodes = nodes.reduce((memo, node) => memo.concat(textNodesUnder(node)), [] as Node[]);
 
     textNodes.forEach(node => {
       let text = node.textContent || '';
@@ -47,11 +54,10 @@ export function applyColourToPanels(panels: PanelDefinition<PossiblyEncodedGraph
 
         const partWrapperNode = document.createElement('span');
         const electorateID = ElectorateID[electorate.id];
-        const { allocations, relative } = data as GraphicProps;
+        const { allocations, certainties } = data as GraphicProps;
         const allocation = allocations && allocations[electorateID];
         const hasDefinitiveAllocation = allocation && determineIfAllocationIsDefinitive(allocation);
-        const relativeAllocations = relative && ELECTORATES_HELD_ALLOCATIONS;
-        const relativeAllocation = relativeAllocations && relativeAllocations[electorateID];
+        const certainty = certainties && certainties[electorateID];
 
         if (!electorateIntroductionTracker[electorateID]) {
           electorateIntroductionTracker[electorateID] = true;
@@ -66,8 +72,8 @@ export function applyColourToPanels(panels: PanelDefinition<PossiblyEncodedGraph
           partWrapperNode.setAttribute('data-has-definitive-allocation', '');
         }
 
-        if (relativeAllocation) {
-          partWrapperNode.setAttribute('data-relative-allocation', relativeAllocation);
+        if (certainty) {
+          partWrapperNode.setAttribute('data-certainty', certainty);
         }
 
         partWrapperNode.setAttribute('data-electorate', electorateID);
