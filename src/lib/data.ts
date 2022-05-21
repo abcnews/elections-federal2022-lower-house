1e2da329de0428e5cc8ba8f49fa9d086b1a786ac
Colin Gourlay <colin@colin-gourlay.com>
2022-05-21 17:54:32 +1000

    Update live results data endpoint

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,27 +1,7 @@
 import type { ElectorateID } from './constants';
 import { Allocation, NoYes } from './constants';
 
-const LIVE_RESULTS_URL_PREFIX =
-  'https://www.abc.net.au/news-web/api/loader/channelrefetch?name=ElectionElectorateList&props=';
-
-const FEDERAL_2019_LIVE_RESULTS_PROPS = {
-  meta: {
-    year: '2019',
-    state: 'federal',
-    remoteContentPath: 'https:%2F%2Fwww.abc.net.au%2Fdat%2Fnews%2Felections%2Ffederal%2F2019'
-  }
-};
-
-const FEDERAL_2022_LIVE_RESULTS_PROPS = {
-  meta: {
-    year: '2022',
-    state: 'federal',
-    useV3: true
-  }
-};
-
-// TODO: update to 2022 when available
-const LIVE_RESULTS_PROPS = FEDERAL_2022_LIVE_RESULTS_PROPS;
+const LIVE_RESULTS_URL = 'https://www.abc.net.au/news-web/api/syndicate/storylab/elections/federal/2022';
 
 interface VoteCount {
   votes: number;
@@ -54,14 +34,12 @@ const liveResultsElectoratesPromises: {
 } = {};
 
 export const fetchLiveResultsElectorates = async () => {
-  // const url = `${LIVE_RESULTS_URL_PREFIX}${JSON.stringify(LIVE_RESULTS_PROPS)}`;
-  const url = `${__webpack_public_path__}example-data/2019-aurora.json`;
-  // const url = `${__webpack_public_path__}example-data/2019-erads.json`;
+  const url = LIVE_RESULTS_URL;
 
   if (!liveResultsElectoratesPromises[url]) {
     liveResultsElectoratesPromises[url] = fetch(url)
       .then(response => response.json())
-      .then(({ results }) => results.electorates as LiveResultsElectorate[]);
+      .then(({ data }) => data.electorates as LiveResultsElectorate[]);
   }
 
   return liveResultsElectoratesPromises[url];
